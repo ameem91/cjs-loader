6965e73bcbf3ef4edc139101b18e7ecaedee906c
Ameem Shaik <ashaik@ebsco.com>
2018-08-05 00:32:50 -0400

    add pathHelper tests

---
+++ test/pathHelper.test.js
@@ -0,0 +1,29 @@
+const PathHelper = require("../src/pathHelper");
+
+describe("PathHelper", () => {
+  describe("resolvePath()", () => {
+    it("should return the given path when it is already an absolute path", () => {
+      const mockPath = "/path/to/file.js";
+      expect(PathHelper.resolvePath(mockPath)).toEqual(mockPath);
+    });
+
+    it("should convert relative paths to absolute paths using the parent as baseline when available", () => {
+      const mockParentModule = {
+        absolutePath: "/path/to/my/file.js"
+      };
+
+      const mockPath = "../../anotherFile.js";
+      expect(PathHelper.resolvePath(mockPath, mockParentModule)).toEqual(
+        "/path/anotherFile.js"
+      );
+    });
+
+    it("should covert relative paths to absolute using the cwd as baseline if no parent is available", () => {
+      const mockPath = "../../anotherFile.js";
+      process.cwd = jest.fn();
+      process.cwd.mockReturnValue("/path/to/mycwd/");
+      expect(PathHelper.resolvePath(mockPath)).toEqual("/path/anotherFile.js");
+      process.cwd.restore();
+    });
+  });
+});
